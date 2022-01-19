a5387b561d0b517af4da89f76655515b6074ce93
Arman Chhetri <armanchhetri44@gmail.com>
2022-01-19 18:36:39 +0545

    validation and logging

--- backend/app/crud/evaluation.py
+++ backend/app/crud/evaluation.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException
 from typing import Tuple
 from cachetools import cached, TTLCache
 from app.core.config import settings
@@ -7,15 +8,40 @@ cache = TTLCache(maxsize=50, ttl=86400)
 import pandas as pd
 from sklearn.metrics import f1_score
 
+def validate_dataset(df_sol, df_sub):
+    if df_sub.isnull().values.any():
+        raise HTTPException(
+            status_code=400, detail="Null value found")
+
+    if (df_sub.columns.tolist() != df_sol.columns.to_list()):
+        raise HTTPException(
+            status_code=400, detail="Columns mismatch")
+        
+    if (df_sol.shape != df_sub.shape):
+        raise HTTPException(
+            status_code=400, detail="Improper solution length")
+        
+    try:
+        if sum(df_sol["id"].to_numpy() == df_sub["id"].to_numpy()) != df_sol.shape[0]:
+            raise HTTPException(status_code=400, detail="Id mismatch")
+    except:
+        raise HTTPException(status_code=400, detail="Submission format does not match with sample please verify")
+
 @cached(cache)
 def calculate_score(file_path: str) -> Tuple[float, float]:
-    df = pd.read_csv(file_path, index_col='id')
-    sol_df = pd.read_csv(settings.SOLUTION_PATH, index_col='id')
-    true = sol_df['category_num']
-    pred = df['category_num']
+    df_sub = pd.read_csv(file_path)
+    df_sub = df_sub.sort_values(by=["id"])
+
+    df_sol = pd.read_csv(settings.SOLUTION_PATH)
+    df_sol = df_sol.sort_values(by=["id"])
+
+    validate_dataset(df_sol, df_sub)
+
+    true = df_sol['category_num']
+    pred = df_sub['category_num']
     private_score = f1_score(true, pred, average='macro')
-    index = int(0.9 * len(true))
-    pub_true = sol_df['category_num'][:index]
-    pub_pred = df['category_num'][:index]
+    index = int(0.8 * len(true))
+    pub_true = df_sol['category_num'][:index]
+    pub_pred = df_sub['category_num'][:index]
     pub_score = f1_score(pub_true, pub_pred, average='macro')
     return pub_score, private_score
